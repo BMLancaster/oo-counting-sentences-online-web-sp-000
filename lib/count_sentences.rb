3a7d3eeec6c5777ed48d4e56fd4ded1dfc3da288
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-01-07 21:34:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,11 @@ class String
   end
 
   def count_sentences
-  self.split(/[.!?]/).reject {
+  self.split(/[.!?]/).reject {|x| x.empty?}.size
+end
+
+def count_sentences_words
+  self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil)}.reject{|x| x == false}.size
+  
   end
 end
